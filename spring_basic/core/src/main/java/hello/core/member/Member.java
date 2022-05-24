09059bbfe2a48d210d2bbf2a644e8387ae004d67
JongwonAhn <azsx1001202@naver.com>
2022-05-24 23:56:55 +0900

    firstCommit

---
+++ spring_basic/core/src/main/java/hello/core/member/Member.java
@@ -0,0 +1,38 @@
+package hello.core.member;
+
+public class Member {
+
+    private Long id;
+    private String name;
+    private String grade;
+
+    public Member(Long id, String name, String grade) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+}
