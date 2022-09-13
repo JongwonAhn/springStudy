b20992b7a14b31e3cedcfafc6dbd7bb23a869346
JongwonAhn <azsx1001202@naver.com>
2022-09-13 22:56:52 +0900

    'springStudy'

--- spring_basic/core/src/main/java/hello/core/member/Member.java
+++ spring_basic/core/src/main/java/hello/core/member/Member.java
@@ -1,12 +1,13 @@
 package hello.core.member;
 
-public class Member {
+public class
+Member {
 
     private Long id;
     private String name;
-    private String grade;
+    private Grade grade;
 
-    public Member(Long id, String name, String grade) {
+    public Member(Long id, String name, Grade grade) {
         this.id = id;
         this.name = name;
         this.grade = grade;
@@ -20,7 +21,7 @@ public class Member {
         return name;
     }
 
-    public String getGrade() {
+    public Grade getGrade() {
         return grade;
     }
 
@@ -32,7 +33,7 @@ public class Member {
         this.name = name;
     }
 
-    public void setGrade(String grade) {
+    public void setGrade(Grade grade) {
         this.grade = grade;
     }
 }
--- spring_basic/core/src/main/java/hello/core/member/MemoryMemberRepository.java
+++ spring_basic/core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -3,7 +3,7 @@ package hello.core.member;
 import java.util.HashMap;
 import java.util.Map;
 
-public class MemoryMemberRepository implements MemberRepository {
+public class  MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();
 
