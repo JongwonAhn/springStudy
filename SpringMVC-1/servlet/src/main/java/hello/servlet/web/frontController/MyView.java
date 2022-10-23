168f0491f870db63b11c1a0d8632bc2a5fba91be
JongwonAhn <azsx1001202@naver.com>
2022-10-24 02:15:34 +0900

    'springMVC1'

---
+++ SpringMVC-1/servlet/src/main/java/hello/servlet/web/frontController/MyView.java
@@ -0,0 +1,31 @@
+package hello.servlet.web.frontController;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+public class MyView {
+    private String ViewPath;
+
+    public MyView(String viewPath) {
+        ViewPath = viewPath;
+    }
+
+    public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        RequestDispatcher dispatcher = request.getRequestDispatcher(ViewPath);
+        dispatcher.forward(request, response);
+    }
+
+    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        modelToRequestAttribute(model, request);
+        RequestDispatcher dispatcher = request.getRequestDispatcher(ViewPath);
+        dispatcher.forward(request, response);
+    }
+
+    private static void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest request) {
+        model.forEach((key, value) -> request.setAttribute(key, value));
+    }
+}
---
+++ SpringMVC-1/servlet/src/main/java/hello/servlet/web/frontController/v3/ControllerV3.java
@@ -0,0 +1,11 @@
+package hello.servlet.web.frontController.v3;
+
+import hello.servlet.web.frontController.ModelView;
+import hello.servlet.web.frontController.MyView;
+
+import java.util.Map;
+
+public interface ControllerV3  {
+
+    ModelView process(Map<String, String> paramMap);
+}
